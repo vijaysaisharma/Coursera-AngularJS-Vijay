57775269c7a88290dd92b896d226083688d40697
Sharma <vijay-sai.sharma@capgemini.com>
2024-05-09 19:04:37 +0530

    added functions and ng-model

--- app.js
+++ app.js
@@ -8,6 +8,27 @@
     Assignment1Controller.$inject = ['$scope'];
 
     function Assignment1Controller($scope){
-        $scope.message = "Assignment1Message";
+        $scope.message = "";
+
+        $scope.clear = function(){
+            $scope.message = "";
+        };
+
+        $scope.checkIfTooMuch = function(){
+            $scope.message = "";
+            if($scope.numberOfItems === undefined || $scope.numberOfItems === "")
+                {
+                    $scope.message = "Please enter data first";
+                    return;
+                }
+            var items = $scope.numberOfItems.split(',');
+
+            if(items.length <= 3){
+                    $scope.message = "Enjoy!";
+                }
+                else{
+                    $scope.message = "Too much!";
+                }
+        };
     };
 })();
